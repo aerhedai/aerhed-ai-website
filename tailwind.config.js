6465f92c88fa61bcac2eea0beb388d19385e376a
rohan5046 <rohanp@live.co.uk>
2025-07-15 19:10:50 +0100

    Fix config files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Tailwind needs to scan these files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
